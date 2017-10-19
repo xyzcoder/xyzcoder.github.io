021b44e995726489b7a45f797dca2e29734c43ee
Pavan Kumar Aryasomayajulu <pavan@sails.solutions>
2017-10-19 22:24:41 +0530

    aa

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -65,7 +65,6 @@ class Header extends React.Component {
             <div className="row">
                <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                   <div className="site-heading">
-                      <h1>XyzCoder.github.io</h1>
                      <span className="subheading">Collection of my learnings - Pavan Kumar Aryasomayajulu</span>
                      <span className="subheading">Contact Me - pavan.aryasomayajulu@gmail.com</span>
                   </div>
