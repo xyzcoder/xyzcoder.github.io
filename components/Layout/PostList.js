6a7b5055882a1e3c186fb6231851ef841fce08e6
Pavan Kumar Aryasomayajulu <pavan@sails.solutions>
2017-11-15 16:58:30 +0530

    aaa

--- components/Layout/PostList.js
+++ components/Layout/PostList.js
@@ -9,11 +9,37 @@
  */
 
 import React from 'react';
+import fire from './fire';
+import PostTitle from './PostTitle';
 
 class PostList extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        postTitles:[]
+      }
+  };
+
   componentDidMount() {
-    
+  }
+  componentWillMount(){
+    var that = this;
+      let postTitles = fire.database().ref('blog/posttitles');
+
+      postTitles.on('value', function(titles) {
+         for (var key in titles.val()) {
+              if (titles.val().hasOwnProperty(key)) {
+                   that.setState({
+                    postTitles: that.state.postTitles.concat([ {id:key,title:titles.val()[key].title,summary:titles.val()[key].summary}   ])
+                  }) ;
+              }
+          }
+      }, function (error) {
+         console.log("Error: " + error.code);
+      });
+
+      
   }
 
   componentWillUnmount() {
@@ -23,12 +49,35 @@ class PostList extends React.Component {
   render() {
 
     return (
-        <div className="row">
-          <div className="col-lg-3 col-lg-offset-9 col-md-10 col-md-offset-1">
-                <div id='gsearch'>
-                  this is post list page
-                </div>
-          </div>
+        <div className="col-lg-9 col-md-10">
+
+        <div className="menu-list">
+            <div id="menu-content">
+
+              {
+                this.state.postTitles.map(function(title) {
+                    return (
+                      <div>
+                        <div className="post-preview">
+                          <a href={'/post/' + title.id}>
+                              <h3>
+                                  {title.title}
+                              </h3>
+                          </a>
+                        </div>
+                        <div className="row">
+                              <div className="col-lg-12 col-md-10 postData">
+                               <div dangerouslySetInnerHTML={{ __html: title.summary }} />
+                              </div>
+                          </div>
+                      </div>
+                    )
+                })
+              }
+
+            </div>
+        </div>
+
         </div>
     );
   }
