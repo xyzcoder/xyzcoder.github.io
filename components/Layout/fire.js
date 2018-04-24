366e2865bc073f4a151e717b6ea6854533da45fe
Pavan Kumar Aryasomayajulu <pavan@sails.solutions>
2018-04-24 14:10:28 +0530

    Jekyll

--- components/Layout/fire.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase'
-var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-  apiKey: "AIzaSyDKDi7cfb9-MQtr-S_JpXfMoTZSgGLdm18",
-  authDomain: "xyzcoder-887ac.firebaseapp.com",
-  databaseURL: "https://xyzcoder-887ac.firebaseio.com",
-  projectId: "xyzcoder-887ac"
-};
-var fire = firebase.initializeApp(config);
-export default fire;
