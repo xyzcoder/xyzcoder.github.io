da27317e3ba2084fecfd7917ace068b6477a2cdf
Pavan Kumar Aryasomayajulu <pavan.aryasomayajulu@gmail.com>
2018-05-09 18:31:50 +0530

    aa

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -420,15 +420,15 @@ jQuery(document).ready(function($) {
 
     // $('.tabs-profile').click(function() {
 
-    //     $('.skillbar').each(function() {
-    //         $(this).find('.skillbar-bar').width(0);
-    //     });
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
 
-    //     $('.skillbar').each(function() {
-    //         $(this).find('.skillbar-bar').animate({
-    //             width: $(this).attr('data-percent')
-    //         }, 2000);
-    //     });
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
 
     // });
 
