029a863497a74d816c4ffd02a61860d7c1511499
Pavan Kumar Aryasomayajulu <pavan.aryasomayajulu@gmail.com>
2019-01-27 22:25:31 +0530

    misc

--- RotatingIP/RequestViaProxy.py
+++ RotatingIP/RequestViaProxy.py
@@ -161,4 +161,5 @@ if __name__ == '__main__':
 
     while True:
         start = time.time()
-        request1 = req_proxy.generate_proxied_request(test_url)
+        request1 = req_proxy.generate_proxied_request('https://facebook.com')
+        a = ""
