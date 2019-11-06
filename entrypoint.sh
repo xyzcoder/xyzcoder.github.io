7f1281fd6dfd089162b59da9a41efa4a3e553f47
Pavan Kumar Aryasomayajulu <pavan.aryasomayajulu@gmail.com>
2019-11-06 17:02:27 +0530

    aa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Fetching from git"
+git reset --hard HEAD
 git fetch https://github.com/xyzcoder/xyzcoder.github.io.git
 git pull https://github.com/xyzcoder/xyzcoder.github.io.git
