b1a500eae2840c7d00fc0fe738b727d7dd5881ad
xyzcoder <xyzcoder1989@gmail.com>
2019-11-10 11:28:04 +0000

    Automatic update of modified files

--- _site/entrypoint.sh
+++ _site/entrypoint.sh
@@ -2,6 +2,12 @@
 
 echo "Fetching from git using username $GIT_Username"
 
+# Set git to use the credential memory cache
+git config --global credential.helper cache
+
+# Set the cache to timeout after 1 hour (setting is in seconds)
+git config --global credential.helper 'cache --timeout=36000'
+
 git config --global user.email "xyzcoder1989@gmail.com"
 git config --global user.name "xyzcoder"
 git config --global user.password $GIT_Password
